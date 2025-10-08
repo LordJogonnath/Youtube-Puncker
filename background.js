7cd478aa107344bb1349d5a2d94c2136efc4cff8
Jagannathkrishnan Nair <65849067+LordJogonnath@users.noreply.github.com>
2025-10-08 13:05:53 +1100

    Add files via upload

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status !== 'complete') return;
+  
+  const url = tab.url || '';
+
+  if (tab.incognito && url.includes('youtube.com')) {
+    chrome.tabs.update(tabId, { url: 'http://localhost:8000/blocked.html' });
+    return;
+  } 
+
+  if (!tab.incognito && (url === 'https://www.youtube.com/' || url === 'https://www.youtube.com')) {
+    chrome.tabs.update(tabId, { url: 'https://www.youtube.com/feed/subscriptions' });
+  }
+});
---
+++ punck script.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "C:\YA GAT PUNCKED!"
+start python -m http.server 8000
+exit
